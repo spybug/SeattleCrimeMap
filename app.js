776b9bff93ae3de7dc18e1b28c550fa4a7a3b9fe
Karl Ingraham <kingraham9@gmail.com>
2024-04-13 11:22:04 -0700

    Small improvements to overall experience.
    - Added favicon to website
    - Changed font of text and made more responsive
    - Added link to github for more info about site.
    - Modified markers to contain better formatting of info

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ let relativeYears = 3; // years to search back
 let markers = [];
 let bounds = [];
 let markerGroup;
+let mapClickDisabled = false;
 
 function onMapClick(e) {
-    if (markers.length === 0) {
-        querySeattleSocrata(e.latlng.lat, e.latlng.lng);
-    }
+  // Prevent clicking too many times and doing lots of requests.
+  if (!mapClickDisabled) {
+    querySeattleSocrata(e.latlng.lat, e.latlng.lng);
+    mapClickDisabled = true;
+    setTimeout(function () {
+      mapClickDisabled = false;
+    }, 1000)
+  }
 }
 
 /*
@@ -35,162 +41,151 @@ _100_block_address: "18XX BLOCK OF 38TH AVE E"
  */
 
 function querySeattleSocrata(lat, lng) {
-    // Build boundaries of query
-    const max = addToLatLng(lat, lng, radius, radius);
-    const min = addToLatLng(lat, lng, -radius, -radius);
+  // Build boundaries of query
+  const max = addToLatLng(lat, lng, radius, radius);
+  const min = addToLatLng(lat, lng, -radius, -radius);
 
-    //const datequery = `AND to_fixed_timestamp( '2020-01-01T00:00:00Z' ) <
-    // to_fixed_timestamp( offense_start_datetime , '%Y-%m-%d %H:%M:%S' )`
-    const where = `$where=latitude <= ${max.lat} AND longitude <= ${max.lng} ` +
-        `AND latitude >= ${min.lat} AND longitude >= ${min.lng}`;
-    const order = '$order=offense_start_datetime DESC'
-    const query = SOCRATA_ENDPOINT + '?' + where + '&' + order;
+  //const datequery = `AND to_fixed_timestamp( '2020-01-01T00:00:00Z' ) <
+  // to_fixed_timestamp( offense_start_datetime , '%Y-%m-%d %H:%M:%S' )`
+  const where = `$where=latitude <= ${max.lat} AND longitude <= ${max.lng} ` +
+    `AND latitude >= ${min.lat} AND longitude >= ${min.lng}`;
+  const order = '$order=offense_start_datetime DESC'
+  const query = SOCRATA_ENDPOINT + '?' + where + '&' + order;
 
-    console.log(query);
-    console.log(encodeURI(query));
-    fetch(encodeURI(query))
-        .then(res => res.json())
-        .then((events) => {
-            const eventsMap = processEvents(events);
-            makeMarkers(eventsMap);
-            makeBounds(min, max);
-        })
-        .catch(err => { throw err });
+  console.log(query);
+  console.log(encodeURI(query));
+  fetch(encodeURI(query))
+    .then(res => res.json())
+    .then((events) => {
+      const eventsMap = processEvents(events);
+      makeMarkers(eventsMap);
+      makeBounds(min, max);
+    })
+    .catch(err => { throw err });
 }
 
 function filterEvents(events, relativeYears) {
-    let output = [];
-    events.forEach(event => {
-        const startDate = moment(event.offense_start_datetime);
-        const minDate = moment().subtract(relativeYears, 'years');
-        if (startDate >= minDate) {
-            output.push(event);
-        }
-    })
-    return output;
+  let output = [];
+  events.forEach(event => {
+    const startDate = moment(event.offense_start_datetime);
+    const minDate = moment().subtract(relativeYears, 'years');
+    if (startDate >= minDate) {
+      output.push(event);
+    }
+  })
+  return output;
 }
 
 // Returns a map of event report_numbers to the events.
 function processEvents(events) {
-    // First filter out any events not expected.
-    events = filterEvents(events, relativeYears);
+  // First filter out any events not expected.
+  events = filterEvents(events, relativeYears);
 
-    // Stores all events groupd by the report_number
-    let eventMap = {};
-    events.forEach(event => {
-       if (eventMap[event.report_number]) {
-           eventMap[event.report_number].push(event);
-       } else {
-           eventMap[event.report_number] = [event];
-       }
-    });
-    return eventMap;
+  // Stores all events groupd by the report_number
+  let eventMap = {};
+  events.forEach(event => {
+    if (eventMap[event.report_number]) {
+      eventMap[event.report_number].push(event);
+    } else {
+      eventMap[event.report_number] = [event];
+    }
+  });
+  return eventMap;
 }
 
 function makeMarkers(eventMap) {
-    console.log(eventMap);
-    markers.forEach(marker => map.removeLayer(marker));
-    markers = [];
+  console.log(eventMap);
+  markers.forEach(marker => map.removeLayer(marker));
+  markers = [];
 
-    if (markerGroup) {
-        map.removeLayer(markerGroup);
+  if (markerGroup) {
+    map.removeLayer(markerGroup);
+  }
+  markerGroup = L.markerClusterGroup({
+    spiderfyDistanceMultiplier: 1.5
+  });
+
+  for (const eventReportNumber in eventMap) {
+    let newMarker;
+
+    // Always grab first event, to limit duplication of reports with same report number.
+    const event = eventMap[eventReportNumber][0];
+    let eventDateTime = moment(event.offense_start_datetime).format("lll");
+    let title = event.offense + ' - ' + eventDateTime;
+    newMarker = L.marker([event.latitude, event.longitude], { title: title });
+    newMarker.bindPopup(`<b alt>${event.offense}</b><br>Time: ${eventDateTime}<br>Report Number: ${event.report_number}<br>Block: ${event._100_block_address}`)
+
+    if (newMarker) {
+      markers.push(newMarker);
+      markerGroup.addLayer(newMarker);
     }
-    markerGroup = L.markerClusterGroup();
+  }
 
-
-    for (const eventReportNumber in eventMap) {
-        let newMarker;
-
-        if (eventMap[eventReportNumber].length === 1) {
-            const event = eventMap[eventReportNumber][0];
-            newMarker = L.marker([event.latitude, event.longitude], {title: eventReportNumber});
-            newMarker.bindPopup(`<b alt>${event.offense} - ${eventReportNumber}</b><br>${event.offense_start_datetime}<br>${event._100_block_address}`)
-        } else {
-            let offenseStrings = '';
-            for (const event of eventMap[eventReportNumber]) {
-                if (!newMarker) {
-                    newMarker = L.marker([event.latitude, event.longitude], {title: eventReportNumber})
-                } else {
-                    offenseStrings += `<hr>`
-                }
-                offenseStrings += `<b>${event.offense} - ${eventReportNumber}</b><br>${event.offense_start_datetime}<br>${event._100_block_address}`
-            }
-            if (newMarker) {
-                newMarker.bindPopup(offenseStrings);
-            }
-        }
-
-        if (newMarker) {
-            markers.push(newMarker);
-            markerGroup.addLayer(newMarker);
-        }
-    }
-
-    map.addLayer(markerGroup);
+  map.addLayer(markerGroup);
 }
 
 function makeBounds(minLatLng, maxLatLng) {
-    bounds.forEach(bound => map.removeLayer(bound));
-    bounds = [];
+  bounds.forEach(bound => map.removeLayer(bound));
+  bounds = [];
 
-    let newBound = L.rectangle([minLatLng, maxLatLng], {color: "#ff6e6e", weight: 3, fillOpacity: 0.15, interactive: false});
-    bounds.push(newBound);
-    map.flyToBounds(newBound, { padding: [48, 48], duration: 0.5 });
-    newBound.addTo(map)
+  let newBound = L.rectangle([minLatLng, maxLatLng], { color: "#ff6e6e", weight: 3, fillOpacity: 0.15, interactive: true });
+  bounds.push(newBound);
+  map.flyToBounds(newBound, { padding: [48, 48], duration: 0.5 });
+  newBound.addTo(map)
 }
 
 
 function init() {
-    map = L.map('mapid').setView([47.6072, -122.3321], 12);
+  map = L.map('mapid').setView([47.6072, -122.3321], 12);
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoiYXN5bmNsaW5rIiwiYSI6ImNraHd5cm5jOTAwNnUyeHBjeXJrOHB5NTkifQ.v5a6KbFQHti809og2pxSRw'
-    }).addTo(map);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 20,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiYXN5bmNsaW5rIiwiYSI6ImNraHd5cm5jOTAwNnUyeHBjeXJrOHB5NTkifQ.v5a6KbFQHti809og2pxSRw'
+  }).addTo(map);
 
-    // Map handlers
-    map.on('click', onMapClick);
+  // Map handlers
+  map.on('click', onMapClick);
 }
 
 // Returns the distance between two lat lng points in feet.
 function distance(lat1, lng1, lat2, lng2) {
-    if ((lat1 === lat2) && (lng1 === lng2)) {
-        return 0;
+  if ((lat1 === lat2) && (lng1 === lng2)) {
+    return 0;
+  }
+  else {
+    const radlat1 = Math.PI * lat1 / 180;
+    const radlat2 = Math.PI * lat2 / 180;
+    const theta = lng1 - lng2;
+    const radtheta = Math.PI * theta / 180;
+    let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
     }
-    else {
-        const radlat1 = Math.PI * lat1/180;
-        const radlat2 = Math.PI * lat2/180;
-        const theta = lng1-lng2;
-        const radtheta = Math.PI * theta/180;
-        let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-        if (dist > 1) {
-            dist = 1;
-        }
-        dist = Math.acos(dist);
-        dist = dist * 180/Math.PI;
-        dist = dist * 60 * 1.1515 * 5280;
-        return dist;
-    }
+    dist = Math.acos(dist);
+    dist = dist * 180 / Math.PI;
+    dist = dist * 60 * 1.1515 * 5280;
+    return dist;
+  }
 }
 
 // https://stackoverflow.com/questions/7477003/calculating-new-longitude-latitude-from-old-n-meters
-function addToLatLng(lat, lng, feetY=0, feetX=0) {
-    const earth = 6378.137; // radius of the earth in kilometers
-    const meterInDegree = (1 / ((2 * Math.PI / 360) * earth)) / 1000;  // 1 meter in degree
+function addToLatLng(lat, lng, feetY = 0, feetX = 0) {
+  const earth = 6378.137; // radius of the earth in kilometers
+  const meterInDegree = (1 / ((2 * Math.PI / 360) * earth)) / 1000;  // 1 meter in degree
 
-    const metersY = feetY / 3.2808;
-    const metersX = feetX / 3.2808;
+  const metersY = feetY / 3.2808;
+  const metersX = feetX / 3.2808;
 
-    const newLat = lat + (metersY * meterInDegree);
-    const newLng = lng + (metersX * meterInDegree) / Math.cos(lat * (Math.PI / 180));
-    return L.latLng(newLat, newLng);
+  const newLat = lat + (metersY * meterInDegree);
+  const newLng = lng + (metersX * meterInDegree) / Math.cos(lat * (Math.PI / 180));
+  return L.latLng(newLat, newLng);
 }
 
 
-window.onload = function(){
-    init();
+window.onload = function () {
+  init();
 };
